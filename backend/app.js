ed6ae6f58925d23028cfef0acf53f766e4937923
AlexNuamah <alexandernuamahjr@gmail.com>
2023-12-15 15:05:33 +0000

    Update: database connection

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import helmet from "helmet";
+import sequelize from "./config/dbConnect.js";
 
 const PORT = process.env.PORT || 2001;
 const app = express();
 app.use(helmet)
 
-app.listen(PORT, ()=>{
+app.listen(PORT, async ()=>{
     try {
-        console.log(`Server is running on http://localhost:${PORT}`);
+        await sequelize.authenticate()
+        console.log(`Server is running on http://localhost:${PORT}`)
     } catch (error) {
         console.log(error);
     }
