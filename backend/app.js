7db050073a02d12cbe3e103ad26f8ff256897aab
AlexNuamah <alexandernuamahjr@gmail.com>
2023-12-17 12:48:18 +0000

    Update: routes entry point

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import helmet from "helmet";
+import userRoute from "./routes/userRoute.js";
 import sequelize from "./config/dbConnect.js";
 
 const PORT = process.env.PORT || 2001;
 const app = express();
-app.use(helmet)
+app.use(helmet);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/", userRoute);
 
 app.listen(PORT, async ()=>{
     try {
